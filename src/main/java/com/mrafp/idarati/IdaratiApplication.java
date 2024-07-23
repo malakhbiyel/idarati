bde4fa60d6f88e05ffc1acd8d0d56ceb4634d4fb
malakhbiyel <malak_hbiyel@outlook.fr>
2024-07-23 15:36:14 +0100

    first commit

---
+++ src/main/java/com/mrafp/idarati/IdaratiApplication.java
@@ -0,0 +1,14 @@
+package com.mrafp.idarati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdaratiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(IdaratiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrafp/idarati/IdaratiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrafp.idarati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdaratiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
